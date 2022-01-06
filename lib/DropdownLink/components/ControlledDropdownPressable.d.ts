a2fe54b5bcf21dd2475ff1c75127f1a090ac9993
Mikey Lau <mikey.lau@traininblocks.com>
2022-01-06 15:10:18 +0000

    feat(elements): hide label on selectedand delimited for multiple option dropdowns

--- lib/DropdownLink/components/ControlledDropdownPressable.d.ts
+++ lib/DropdownLink/components/ControlledDropdownPressable.d.ts
@@ -1,3 +1,3 @@
 import { DropdownLinkProps } from '..';
-declare const ControlledDropdownPressable: ({ dropdown, setDropdown, control, validation, setValue, disabled, navigateTo, onPress, placeholderStyle, labelStyle, textStyle, bgColor, borderColor, icon, iconColor, iconSize, iconStyle, iconName, }: DropdownLinkProps) => JSX.Element;
+declare const ControlledDropdownPressable: ({ dropdown, setDropdown, control, validation, setValue, disabled, hideLabelSelected, delimiter, navigateTo, onPress, placeholderStyle, labelStyle, textStyle, bgColor, borderColor, icon, iconColor, iconSize, iconStyle, iconName, }: DropdownLinkProps) => JSX.Element;
 export default ControlledDropdownPressable;
